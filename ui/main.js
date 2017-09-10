649873904763721bbcce9d8c727942a891074255
umi <mubaenn001@gmail.com>
2017-09-10 13:41:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,12 @@ var element=document.getElementById("main-text");
 element.innerHTML="New value";
 //move the image
 var img=document.getElementById('madi');
-madi.onclick=function() {
-  madi.style.marginLeft='100px';  
+var marginLeft=0;
+function moveRight (){
+   marginLeft=marginLeft+10;
+   img.style.marginLeft=marginLeft+'px';
+}
+img.onclick=function() {
+    var interval=setInterval(moveRight,100);
+        
 };
