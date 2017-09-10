92251975915e2745a447704ac6c0eec1d668ecd2
umi <mubaenn001@gmail.com>
2017-09-10 13:29:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 //change the value of main-text div
 var element=document.getElementById("main-text");
 element.innerHTML="New value";
+//move the image
+var img=document.getElementById('img');
+img.omclick=function() {
+  img.style.marginLeft='100px';  
+};
